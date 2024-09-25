2c6064282af9aea0a2e9a502a5e6051e816e47a5
Gerard Byrne <gedbyrned@gmail.com>
2024-09-25 13:44:39 +0100

    small corrections

--- translate-portuguese/src/reportWebVitals.ts
+++ translate-portuguese/src/reportWebVitals.ts
@@ -1,4 +1,6 @@
-const reportWebVitals = onPerfEntry => {
+import { ReportHandler } from 'web-vitals';  // Import the correct type
+
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
